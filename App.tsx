37b1db635e2733216e82671b475da341bbea4bf5
saurav <saurav.maharjan7@gmail.com>
2022-08-10 18:16:06 +0545

    imported navigation native and stack and setup for navigation

--- App.tsx
+++ App.tsx
@@ -1,24 +1,27 @@
 
 
-import React, { type PropsWithChildren } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  Text,
-  View,
-} from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/home';
+import Detail from './screens/detail';
 
+const Stack = createStackNavigator();
 
-const App = () => {
+export default function App() {
 
   return (
-    <SafeAreaView >
-      <ScrollView>
-        <View>
-          <Text>Firebase Crud</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen 
+        name='Home' 
+        component={Home}
+        />
+        <Stack.Screen 
+        name='Detail' 
+        component={Detail} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-export default App;
